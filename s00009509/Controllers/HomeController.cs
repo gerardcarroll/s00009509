7779dde2c7e4ab954a6d1f51ba7ef897de5114a9
gerardcarroll <gercarroll@gmail.com>
2013-11-29 14:43:36 +0000

    finish

--- s00009509/Controllers/HomeController.cs
+++ s00009509/Controllers/HomeController.cs
@@ -40,18 +40,5 @@ namespace s00009509
             return View(allOrders);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
